2b179c0b8642c6aa6c4dd6c14bad7badb41b090b
chingTheCoder <chingliu10@outlook.com>
2021-12-09 11:55:08 -0800

    moving to a=showroom section

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -11,116 +11,35 @@
     </head>
     <body>
         <header>
-        @include("partials/header") 
+        @include("partials/header")
+        <div class="about_header">
+            <h1>ABC EMPORIO TANZANIA</h1>
+            <p>FEATURED WORLD CLASS PRODUCT</p>
+        </div>
         </header>
-        <section id="about_top">
-            <div class="about_top">
-                  <div>
-                    <h2>About Abc</h2>
-                  <p>
-                    ABC GROUP started in December 1998 at Taliparampa, kerala,India.Today abc group 
-                    is the synonym for high quality tiles, sanitary wares, and bathroom fittings and 
-                    allied building materials.Abc group is taking the high road of success for the last 18 years .Today the group is growing in 3 key areas of business i.e customer Base.Supply Chain and Global Expansion
-                    The ABC Group is dedicated to make the world a better place through innovative business 
-                    and inspiring luxury living.Having the largest collection of trend setting tiles, sanitary ware and building and material products by the world's best production houses, mission are serve the customer better and better forever.
-                  </p>
-                  </div>
-            </div>
-            <!----
-            <div class="about_top" style="background-image: url(images/home00.jpg);">
-            </div>
-            --->
-        </section>
-        <section id="message_from_chairman">
-              <div  class="chairman"
-                style="background-image: url('/images/chairman.jpg');background-size: cover;">
+
+        <section class="who_are_we row">
+              <div class="who_are_we_details col-12 col-lg-6 col-xl-6">
+                     <div>
+                     <h2>who are we</h2>
+                      <hr/>
+                      <p>
+                            ABC EMPORIO is a venture of ABC GROUP a synomy for high end sanitary ware, bathroom fittings, tiles & allied buildings
+                            materials.ABC GROUP had its inception in the year 1988 at Taliparamba in kerala, india.Today ABC Group is known as an industry
+                            leader in the business of import and export of varous luxury products & materials around the world
+                      </p>
+                     </div>
+              </div>
+              <div class="who_are_we_image col-12 col-lg-6 col-xl-6">
 
               </div>
-              <div class="chairman">
-                    <div>
-                      <h2>Message from our chairman</h2>
-                      <p>
-                        "The ABC Group is a leader in tiles, sanitary ware and building material business.
-                         Motivated constantly by the patronage of beloved customers, dedication of trustworthy 
-                         employees and the guidance of well wishers. ABC Group is going global. Our aim is to
-                         become a billion dollar company by the year of 2031. ABC Group is expanding to new frontiers and new continents steadfastly. We are commited to deliver superior product quality, unmatched customer satisfaction and enhanced customer promise. With us one can be assured of quality and satisfaction. Am sure the company will grow many fold in the time to come"
-                      </p>
-                    </div>
-              </div>
         </section>
-        <!--message from our chairman-->
-        <section id="bhagh_of_abc">
-            <h3>The BHAGH OF ABC</h3>
-            <P>"To become a billion dollar company by the year of 2031"</P>
-            <hr style="height: 2px;background-color: #111;">
-            <div style="width: 100%;">
-                    <ul>
-                        <li>
-                          A well known as customer centric grobal brand in the building materials
-                        </li>
-                        <li>
-                          A company become the most sought after solutions providing company for our lower end to higher end customers.
-                        </li>
-                        <li>
-                          Become a company providing grobal brand solutions under one roof
-                        </li>
-                        <li>
-                          Become a mojor driving force, bringing revolutionary changes and transforming
-                          the global construction industrys
-                        </li>
-                        <li>
-                          Become one the largest point solution provider for its product, design and execution in the world
-                        </li>
-                      <li>
-                        Becom a world leader with operations in more than 50 countries, with its National Head Quarter in Dubai
-                      </li>
-                      <li>
-                        Directed towards establishing the ABC foundation for charity and empowerment of old aged and differently abled people
-                      </li>
-                      <li>
-                        ABC will contribute 5% of its profits towards charity, empowerment and upliftment of the under priveleged
-                      </li>
-                      <li>
-                        ABC will successfully launch the ABC Academy to build professionally skilled people
-                      </li>
-                    </ul>
-            </div>
-          </section>
-          <div id="values_and_god">
-                      <h3>CORE VALUES</h3>
-                      <ul>
-                          <li>
-                            <span class="core_header">Relationship for lifes</span><br/>
-                            ABC has a lifelong commitment, employees, stake holders, vendors and well-wishers. while .we are committed to help them to lead a happier, longer and fulfilling life.
-                          </li>
-                          <li>
-                            <span class="core_header">Customer service and satisfaction</span><br/>
-                            ABC provides customer experience that is not just the best, but exceptional we relentlessly strive at delivering innovative and enduring products and services that are unmatched thus ansuring absolute customer satisfaction
-                          </li>
-                          <li>
-                            <span class="core_header">Futuristic and innovative</span><br/>
-                            ABC rises up to a fresh world of ideas and a new way of life it proudly brings state of the art designs & products from expertise across the world and spread innovative ideas.
-                          </li>
-                          <li>
-                            <span class="core_header">Teamwork For Excellence</span><br/>
-                            For us Excellence is a habit.we are powered by a team that out grown individual performance and has made excellence reality
-                          </li>
-                          <li>
-                            <span class="core_header">
-                            Responsible & Result Oriented
-                            </span><br/>
-                            We are committed to deliver responsible and result oriented products & services we believe that the success of our organization depends upon its responsibility to the society
-                          </li>
-                          <li>
-                            <span class="core_header">Win-win Co-operation</span><br/>
-                            ABC always achieves success through its unique win-win strategy.Our customers, employees , stakeholders and vendors will reap the benefits of happiness and good life
-                          </li>
-                      </ul>
-                    <img id="hands_image 
-                      "src="images/godfit.png" 
-                        style="display:block;margin:0px auto;max-height: 300px;max-width: 300px;" alt="hands or gods gift">
-                    <h3>GOD'S GIFT</h3>
-                    <ul>
+
+        <section id="gods_gift" class="row">
+                <div class="gift col-12 col-lg-6 col-xl-6">
+                <h4 class="text-center">GOD'S GIFT</h4>
+                <img src="https://abcemporiotz.com/public/images/godfit.png">
+                <ul>
                         <li class="gods_list">
                           I asked for strength
                         </li>
@@ -163,10 +82,76 @@
                         <li class="gods_list">
                           and received everything i need
                         </li>
+                        <li class="gods_list" style="font-weight: bold;">
+                          MY PLAYER HAS BEEN ANSWERED
+                       </li>
                     </ul>
-                    <h3>MY PRAYER HAS BEEN ANSWERED!!</h3>
-          </div>
-          @include("partials/footer") 
+
+                </div>
+               
+                <div class="message col-12 col-lg-6 col-xl-6">
+                        <img src="/images/madini.png" id="madini_image">
+                        <h4>MESSAGE FROM CHAIRMAN</h4>
+                        <p>
+                        "The ABC Group is a leader in tiles, sanitary ware and building material business. Motivated constantly 
+                        by the patronage of beloved customers, dedication of trustworthy employees and the guidance of well wishers. 
+                        ABC Group is going global. Our aim is to become a billion dollar company by the year of 2031. ABC Group is expanding to new frontiers and new continents steadfastly. We are commited to deliver superior product quality, unmatched customer satisfaction and enhanced customer promise. With us one can be assured of quality and satisfaction. Am sure the company will grow many fold in the time to come"
+                        </p>
+                </div>
+        </section>
+
+        <section class="core">
+                <div class="inner_core row">
+                <div class="mstari">
+                  
+                </div>
+                <div class="base_Core">
+                    CORE<br/>
+                    VALUES
+                </div>
+                <div class="purpose col-12 col-lg-6 col-xl-6">
+                      <div>
+                      <h4>CORE PURPOSE<h4>
+                    <h5>"TO HELP PEOPLE TRANSFORM THEIR WORLD"<h5>
+                     <p>
+                       "Every accomplishmentment starts with decision to transform.ABC believes in the power of transforming people's life".
+                     </p>
+                      </div>
+                </div>
+                <div class="values col-12 col-lg-6 col-xl-6">
+                    <h4 class="cc">CORE VALUES<h4>
+                      <ul>
+                          <li>
+                            <span class="core_header">Relationship for lifes</span>
+                            ABC has a lifelong commitment, employees, stake holders, vendors and well-wishers. while .we are committed to help them to lead a happier, longer and fulfilling life.
+                          </li>
+                          <li>
+                            <span class="core_header">Customer service and satisfaction</span>
+                            ABC provides customer experience that is not just the best, but exceptional we relentlessly strive at delivering innovative and enduring products and services that are unmatched thus ansuring absolute customer satisfaction
+                          </li>
+                          <li>
+                            <span class="core_header">Futuristic and innovative</span>
+                            ABC rises up to a fresh world of ideas and a new way of life it proudly brings state of the art designs & products from expertise across the world and spread innovative ideas.
+                          </li>
+                          <li>
+                            <span class="core_header">Teamwork For Excellence</span>
+                            For us Excellence is a habit.we are powered by a team that out grown individual performance and has made excellence reality
+                          </li>
+                          <li>
+                            <span class="core_header">
+                            Responsible & Result Oriented
+                            </span>
+                            We are committed to deliver responsible and result oriented products & services we believe that the success of our organization depends upon its responsibility to the society
+                          </li>
+                          <li>
+                            <span class="core_header">Win-win Co-operation</span>
+                            ABC always achieves success through its unique win-win strategy.Our customers, employees , stakeholders and vendors will reap the benefits of happiness and good life
+                          </li>
+                      </ul>
+                </div>
+                </div>
+        </section>
+            @include("partials/footer") 
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
